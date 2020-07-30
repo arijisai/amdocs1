dde891f3aeded9761d2bc43e31d9801f900a0f56
arijit sai <arijitsai1999@gmail.com>
2020-07-30 08:09:25 +0530

    Revert "added to repo"
    This reverts commit 7368f660a97c38e3ade8cd466869c8357713739a.

--- Abc.java
+++ Abc.java
@@ -2,5 +2,6 @@ public class Abc{
 	public static void main(String args[]){
 		System.out.println("hii arijit sai");
 		System.out.println("welcome to amdocs");
+		System.out.println("Edureka");
 	}
 }
