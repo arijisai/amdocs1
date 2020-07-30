2424ff4db1333d67fbf49567a8e457f82545d98c
arijit sai <arijitsai1999@gmail.com>
2020-07-30 08:26:06 +0530

    hii

--- Abc.java
+++ Abc.java
@@ -1,7 +1,9 @@
 public class Abc{
 	public static void main(String args[]){
+<<<<<<< HEAD
+
+=======
 		System.out.println("hii arijit sai");
-		System.out.println("welcome to amdocs");
-		System.out.println("Edureka");
+>>>>>>> parent of d76d62a... added
 	}
 }
