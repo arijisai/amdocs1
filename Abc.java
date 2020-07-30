91cdcf246163c42ce2d2abecbf07d78a73006fa1
arijit sai <arijitsai1999@gmail.com>
2020-07-30 20:34:03 +0530

    Revert "hii"
    This reverts commit 2424ff4db1333d67fbf49567a8e457f82545d98c.

--- Abc.java
+++ Abc.java
@@ -1,9 +1,7 @@
 public class Abc{
 	public static void main(String args[]){
-<<<<<<< HEAD
-
-=======
 		System.out.println("hii arijit sai");
->>>>>>> parent of d76d62a... added
+		System.out.println("welcome to amdocs");
+		System.out.println("Edureka");
 	}
 }
